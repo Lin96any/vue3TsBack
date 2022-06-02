e306f20b49d02142665d114b9cb56dc54d6bfc5b
林大大 <lin2456015180lq@icloud.com>
2022-06-02 16:14:38 +0800

    feat(新增gitHusky工具): 基于Husky规范git提交规范

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	//继承的规则
+	extends: ['@commitlint/config-conventional'],
+	//自定义规则
+	roles: {
+		'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
+        'subject-case': [0, 'never'],
+    },
+};
